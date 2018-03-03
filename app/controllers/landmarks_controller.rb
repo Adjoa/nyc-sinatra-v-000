2be001e0ed9905889b9aa87b633170e4b191e26a
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-03 18:54:31 +0000

    added get '/landmarks/:id'

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -12,4 +12,9 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'/landmarks/index'
   end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
 end
