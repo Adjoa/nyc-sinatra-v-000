8c7a3ec05b4ad8eb16317e07b6e89202a6061a9b
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-05 00:45:27 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,7 +6,7 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(name: params[:figure][:name])
     @figure.title_ids = params[:figure][:title_ids] if params[:figure][:title_ids]
-    @figure.landmarks = params[:figure][:title_ids]
+    @figure.landmarks = params[:figure][:title_ids] if params[:figure][:title_ids]
     # binding.pry
   end
 
