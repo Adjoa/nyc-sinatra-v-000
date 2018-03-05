bd48a95964a2c76941118f88d8bb5266b20cf998
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-05 00:28:49 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,6 +6,7 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(name: params[:figure][:name])
     @figure.title_ids = params[:figure][:title_ids]
+    @figure.landmark = params[:figure][:landmark]
     # binding.pry
   end
 
