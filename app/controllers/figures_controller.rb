7d2168f4b5e8aefef47e26749de0034414a3627e
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-05 01:51:42 +0000

    patch '/figures/:id' redirects to figure show page

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,7 +33,20 @@ class FiguresController < ApplicationController
   end
 
   patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(name: params[:figure][:name])
+    @figure.title_ids = params[:figure][:title_ids] if params[:figure][:title_ids]
+    @figure.landmark_ids = params[:figure][:landmark_ids] if params[:figure][:landmark_ids]
 
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+
+    redirect "/figures/#{@figure.id}"
   end
 
 end
