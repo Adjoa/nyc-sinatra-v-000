d99c09c19f68bdde2efcfb0db2e461ea73b6e750
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-05 01:01:05 +0000

    post '/figures' allows you to create a new figure with a new title

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,6 +7,9 @@ class FiguresController < ApplicationController
     @figure = Figure.create(name: params[:figure][:name])
     @figure.title_ids = params[:figure][:title_ids] if params[:figure][:title_ids]
     @figure.landmark_ids = params[:figure][:landmark_ids] if params[:figure][:landmark_ids]
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
     # binding.pry
   end
 
